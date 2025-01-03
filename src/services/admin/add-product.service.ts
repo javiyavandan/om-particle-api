70c0a659bb7ca3de31e3948867e6f14f9efd7371
Vihaa-Shreyas <136700117+Vihaa-Shreyas@users.noreply.github.com>
2025-01-03 16:10:28 +0530

    stock list api company id issue fix

--- src/services/admin/add-product.service.ts
+++ src/services/admin/add-product.service.ts
@@ -439,7 +439,7 @@ export const getAllStock = async (req: Request) => {
 
         const where = [
             { is_deleted: DeleteStatus.No },
-            req.body.id_role == 0 ? {} : { company_id: req.body.company_id },
+            req.body.session_res.id_role == 0 ? {} : { company_id: req.body.session_res.company_id },
             pagination.is_active ? { is_active: pagination.is_active } : {},
             pagination.search_text
                 ? {
