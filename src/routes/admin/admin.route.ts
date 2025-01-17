9d0389e8d98e28cdddfe0dc4c2fa1c1849693c65
Vihaa-Shreyas <136700117+Vihaa-Shreyas@users.noreply.github.com>
2025-01-17 17:33:14 +0530

    Update admin.route.ts

--- src/routes/admin/admin.route.ts
+++ src/routes/admin/admin.route.ts
@@ -10,6 +10,7 @@ import {
 } from "../../controllers/admin/admin.controller";
 import { adminAuthorization } from "../../middlewares/authenticate";
 import { updateCustomerFromAdminValidator } from "../../validators/auth/auth.validator";
+import { reqMultiImageParser } from "../../middlewares/multipart-file-parser";
 
 export default (app: Router) => {
   app.get("/users", [adminAuthorization], getAllUser);
@@ -18,5 +19,5 @@ export default (app: Router) => {
   app.patch("/user-status/:user_id", [adminAuthorization], updateUserStatusFn);
   // app.get("/dashboard", [adminAuthorization], dashboardFn);
   app.get("/contact-us", [adminAuthorization], contactUsListFn);
-  app.put("/user/:user_id", [adminAuthorization, updateCustomerFromAdminValidator], updateUserFn); 
+  app.put("/user/:user_id", [adminAuthorization, reqMultiImageParser(["image", "pdf"]), updateCustomerFromAdminValidator], updateUserFn);
 };
