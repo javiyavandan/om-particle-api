3ecf8316d1868612f744799691dc50cff2d1c302
Vihaa-Shreyas <136700117+Vihaa-Shreyas@users.noreply.github.com>
2025-01-20 19:27:13 +0530

    Update stock.service.ts

--- src/services/user/stock.service.ts
+++ src/services/user/stock.service.ts
@@ -23,53 +23,15 @@ export const getStockList = async (req: Request) => {
         const polish = query.polish ? (query.polish as string).split(",").map(id => `${id.trim()}`).join(",") : "";
         const symmetry = query.symmetry ? (query.symmetry as string).split(",").map(id => `${id.trim()}`).join(",") : "";
         const labs = query.lab ? (query.lab as string).split(",").map(id => `${id.trim()}`).join(",") : "";
+        const customer = query.customer ? (query.customer as string).split(",").map(id => `${id.trim()}`).join(",") : "";
         const fluorescence = query.fluorescence ? (query.fluorescence as string).split(",").map(id => `${id.trim()}`).join(",") : "";
 
         const totalItems = await dbContext.query(
             `
                 SELECT
-                    diamond_list.id,
-                    stock_id,
-                    shape,
-                    shape_name,
-                    clarity,
-                    clarity_name,
-                    color,
-                    color_name,
-                    color_intensity,
-                    color_intensity_name,
-                    lab,
-                    lab_name,
-                    polish,
-                    polish_name,
-                    symmetry,
-                    symmetry_name,
-                    fluorescence,
-                    fluorescence_name,
-                    quantity,
-                    weight,
-                    report,
-                    video,
-                    image,
-                    certificate,
-                    measurement_height,
-                    measurement_width,
-                    measurement_depth,
-                    table_value,
-                    depth_value,
-                    ratio,
-                    user_comments,
-                    admin_comments,
-                    local_location,
-                    status,
-                    rate * ${currency} as rate,
-                    company_id,
-                    company_name,
-                    is_active,
-                    wishlist_products.id AS wishlist_id
+                    *
                 FROM
                     diamond_list
-                    ${id ? `LEFT JOIN wishlist_products ON wishlist_products.product_id = diamond_list.id AND wishlist_products.user_id = '${id}'` : ''}
                 WHERE
                 CASE WHEN '${pagination.search_text}' = '0' THEN TRUE ELSE 
                             shape_name ILIKE '%${pagination.search_text}%'
@@ -81,6 +43,11 @@ export const getStockList = async (req: Request) => {
                             OR user_comments ILIKE '%${pagination.search_text}%'
                             OR admin_comments ILIKE '%${pagination.search_text}%'
                             OR ratio ILIKE '%${pagination.search_text}%'
+                            OR customer_name ILIKE '%${pagination.search_text}%'
+                            OR lab_name ILIKE '%${pagination.search_text}%'
+                            OR company_name ILIKE '%${pagination.search_text}%'
+                            OR first_name ILIKE '%${pagination.search_text}%'
+                            OR last_name ILIKE '%${pagination.search_text}%'
                             OR CAST(quantity AS TEXT) ILIKE '%${pagination.search_text}%'
                             OR CAST(weight AS TEXT) ILIKE '%${pagination.search_text}%'
                             OR CAST(rate AS TEXT) ILIKE '%${pagination.search_text}%'
@@ -92,18 +59,18 @@ export const getStockList = async (req: Request) => {
                             OR CAST(measurement_depth AS TEXT) ILIKE '%${pagination.search_text}%'
                         END
                             ${shapes ? `AND shape IN (${shapes})` : ""}
-                            ${colors ? `AND color IN ${colors}` : ""}
-                            ${color_intensity ? `AND color_intensity IN ${color_intensity}` : ""}
-                            ${clarity ? `AND clarity IN ${clarity}` : ""}
-                            ${polish ? `AND polish IN ${polish}` : ""}
-                            ${symmetry ? `AND symmetry IN ${symmetry}` : ""}
-                            ${labs ? `AND lab IN ${labs}` : ""}
-                            ${fluorescence ? `AND fluorescence IN ${fluorescence}` : ""}
-                            ${query.company ? `AND company_id = ${query.company}` : ""}
-                            ${query.status ? `AND status = ${query.status}` : ""}
-                            ${query.min_rate && query.max_rate ? `AND rate * ${currency} BETWEEN ${query.min_rate} AND ${query.max_rate}` : ""}
-                            ${query.min_rate && !query.max_rate ? `AND rate * ${currency} >= ${query.min_rate}` : ""}
-                            ${!query.min_rate && query.max_rate ? `AND rate * ${currency} <= ${query.max_rate}` : ""}
+                            ${colors ? `AND color IN (${colors})` : ""}
+                            ${color_intensity ? `AND color_intensity IN (${color_intensity})` : ""}
+                            ${clarity ? `AND clarity IN (${clarity})` : ""}
+                            ${polish ? `AND polish IN (${polish})` : ""}
+                            ${symmetry ? `AND symmetry IN (${symmetry})` : ""}
+                            ${labs ? `AND lab IN (${labs})` : ""}
+                            ${fluorescence ? `AND fluorescence IN (${fluorescence})` : ""}
+                            ${customer ? `AND customer_id IN (${customer})` : ""}
+                            ${query.status ? `AND status = '${query.status}' ` : ""}
+                            ${query.min_rate && query.max_rate ? `AND rate BETWEEN ${query.min_rate} AND ${query.max_rate}` : ""}
+                            ${query.min_rate && !query.max_rate ? `AND rate >= ${query.min_rate}` : ""}
+                            ${!query.min_rate && query.max_rate ? `AND rate <= ${query.max_rate}` : ""}
                             ${query.min_weight && query.max_weight ? `AND weight BETWEEN ${query.min_weight} AND ${query.max_weight}` : ""}
                             ${query.min_weight && !query.max_weight ? `AND weight >= ${query.min_weight}` : ""}
                             ${!query.min_weight && query.max_weight ? `AND weight <= ${query.max_weight}` : ""}
@@ -122,6 +89,15 @@ export const getStockList = async (req: Request) => {
                             ${query.min_measurement_depth && query.max_measurement_depth ? `AND measurement_depth BETWEEN ${query.min_measurement_depth} AND ${query.max_measurement_depth}` : ""}
                             ${query.min_measurement_depth && !query.max_measurement_depth ? `AND measurement_depth >= ${query.min_measurement_depth}` : ""}
                             ${!query.min_measurement_depth && query.max_measurement_depth ? `AND measurement_depth <= ${query.max_measurement_depth}` : ""}
+                            ${query.start_date && query.end_date
+                ? `AND created_at BETWEEN '${new Date(new Date(query.start_date as string).setMinutes(0, 0, 0)).toISOString()}' AND '${new Date(new Date(query.end_date as string).setMinutes(0, 0, 0)).toISOString()}'`
+                : ""}
+                              ${query.start_date && !query.end_date
+                ? `AND created_at >= '${new Date(new Date(query.start_date as string).setMinutes(0, 0, 0)).toISOString()}'`
+                : ""}
+                              ${!query.start_date && query.end_date
+                ? `AND created_at <= '${new Date(new Date(query.end_date as string).setMinutes(0, 0, 0)).toISOString()}'`
+                : ""}
                 `,
             { type: QueryTypes.SELECT }
         )
@@ -138,48 +114,9 @@ export const getStockList = async (req: Request) => {
         const diamondList = await dbContext.query(
             `
                 SELECT
-                    diamond_list.id,
-                    stock_id,
-                    shape,
-                    shape_name,
-                    clarity,
-                    clarity_name,
-                    color,
-                    color_name,
-                    color_intensity,
-                    color_intensity_name,
-                    lab,
-                    lab_name,
-                    polish,
-                    polish_name,
-                    symmetry,
-                    symmetry_name,
-                    fluorescence,
-                    fluorescence_name,
-                    quantity,
-                    weight,
-                    report,
-                    video,
-                    image,
-                    certificate,
-                    measurement_height,
-                    measurement_width,
-                    measurement_depth,
-                    table_value,
-                    depth_value,
-                    ratio,
-                    user_comments,
-                    admin_comments,
-                    local_location,
-                    status,
-                    rate * ${currency} as rate,
-                    company_id,
-                    company_name,
-                    is_active,
-                    wishlist_products.id AS wishlist_id
+                    *
                 FROM
                     diamond_list
-                    ${id ? `LEFT JOIN wishlist_products ON wishlist_products.product_id = diamond_list.id AND wishlist_products.user_id = '${id}'` : ''}
                 WHERE
                 CASE WHEN '${pagination.search_text}' = '0' THEN TRUE ELSE 
                             shape_name ILIKE '%${pagination.search_text}%'
@@ -191,6 +128,11 @@ export const getStockList = async (req: Request) => {
                             OR user_comments ILIKE '%${pagination.search_text}%'
                             OR admin_comments ILIKE '%${pagination.search_text}%'
                             OR ratio ILIKE '%${pagination.search_text}%'
+                            OR customer_name ILIKE '%${pagination.search_text}%'
+                            OR lab_name ILIKE '%${pagination.search_text}%'
+                            OR company_name ILIKE '%${pagination.search_text}%'
+                            OR first_name ILIKE '%${pagination.search_text}%'
+                            OR last_name ILIKE '%${pagination.search_text}%'
                             OR CAST(quantity AS TEXT) ILIKE '%${pagination.search_text}%'
                             OR CAST(weight AS TEXT) ILIKE '%${pagination.search_text}%'
                             OR CAST(rate AS TEXT) ILIKE '%${pagination.search_text}%'
@@ -199,38 +141,48 @@ export const getStockList = async (req: Request) => {
                             OR CAST(depth_value AS TEXT) ILIKE '%${pagination.search_text}%'
                             OR CAST(measurement_height AS TEXT) ILIKE '%${pagination.search_text}%'
                             OR CAST(measurement_width AS TEXT) ILIKE '%${pagination.search_text}%'
-                            OR CAST(measurement_depth AS TEXT) ILIKE '%${pagination.search_text}%' END
-            ${shapes ? `AND shape IN (${shapes})` : ""}
-            ${colors ? `AND color IN ${colors}` : ""}
-            ${color_intensity ? `AND color_intensity IN ${color_intensity}` : ""}
-            ${clarity ? `AND clarity IN ${clarity}` : ""}
-            ${polish ? `AND polish IN ${polish}` : ""}
-            ${symmetry ? `AND symmetry IN ${symmetry}` : ""}
-            ${labs ? `AND lab IN ${labs}` : ""}
-            ${fluorescence ? `AND fluorescence IN ${fluorescence}` : ""}
-            ${query.company ? `AND company_id = ${query.company}` : ""}
-            ${query.status ? `AND status = ${query.status}` : ""}
-            ${query.min_rate && query.max_rate ? `AND rate * ${currency} BETWEEN ${query.min_rate} AND ${query.max_rate}` : ""}
-            ${query.min_rate && !query.max_rate ? `AND rate * ${currency} >= ${query.min_rate}` : ""}
-            ${!query.min_rate && query.max_rate ? `AND rate * ${currency} <= ${query.max_rate}` : ""}
-            ${query.min_weight && query.max_weight ? `AND weight BETWEEN ${query.min_weight} AND ${query.max_weight}` : ""}
-            ${query.min_weight && !query.max_weight ? `AND weight >= ${query.min_weight}` : ""}
-            ${!query.min_weight && query.max_weight ? `AND weight <= ${query.max_weight}` : ""}
-            ${query.min_depth_value && query.max_depth_value ? `AND depth_value BETWEEN ${query.min_depth_value} AND ${query.max_depth_value}` : ""}
-            ${query.min_depth_value && !query.max_depth_value ? `AND depth_value >= ${query.min_depth_value}` : ""}
-            ${!query.min_depth_value && query.max_depth_value ? `AND depth_value <= ${query.max_depth_value}` : ""}
-            ${query.min_table_value && query.max_table_value ? `AND table_value BETWEEN ${query.min_table_value} AND ${query.max_table_value}` : ""}
-            ${query.min_table_value && !query.max_table_value ? `AND table_value >= ${query.min_table_value}` : ""}
-            ${!query.min_table_value && query.max_table_value ? `AND table_value <= ${query.max_table_value}` : ""}
-            ${query.min_measurement_height && query.max_measurement_height ? `AND measurement_height BETWEEN ${query.min_measurement_height} AND ${query.max_measurement_height}` : ""}
-            ${query.min_measurement_height && !query.max_measurement_height ? `AND measurement_height >= ${query.min_measurement_height}` : ""}
-            ${!query.min_measurement_height && query.max_measurement_height ? `AND measurement_height <= ${query.max_measurement_height}` : ""}
-            ${query.min_measurement_width && query.max_measurement_width ? `AND measurement_width BETWEEN ${query.min_measurement_width} AND ${query.max_measurement_width}` : ""}
-            ${query.min_measurement_width && !query.max_measurement_width ? `AND measurement_width >= ${query.min_measurement_width}` : ""}
-            ${!query.min_measurement_width && query.max_measurement_width ? `AND measurement_width <= ${query.max_measurement_width}` : ""}
-            ${query.min_measurement_depth && query.max_measurement_depth ? `AND measurement_depth BETWEEN ${query.min_measurement_depth} AND ${query.max_measurement_depth}` : ""}
-            ${query.min_measurement_depth && !query.max_measurement_depth ? `AND measurement_depth >= ${query.min_measurement_depth}` : ""}
-            ${!query.min_measurement_depth && query.max_measurement_depth ? `AND measurement_depth <= ${query.max_measurement_depth}` : ""}
+                            OR CAST(measurement_depth AS TEXT) ILIKE '%${pagination.search_text}%'
+                        END
+                            ${shapes ? `AND shape IN (${shapes})` : ""}
+                            ${colors ? `AND color IN (${colors})` : ""}
+                            ${color_intensity ? `AND color_intensity IN (${color_intensity})` : ""}
+                            ${clarity ? `AND clarity IN (${clarity})` : ""}
+                            ${polish ? `AND polish IN (${polish})` : ""}
+                            ${symmetry ? `AND symmetry IN (${symmetry})` : ""}
+                            ${labs ? `AND lab IN (${labs})` : ""}
+                            ${fluorescence ? `AND fluorescence IN (${fluorescence})` : ""}
+                            ${customer ? `AND customer_id IN (${customer})` : ""}
+                            ${query.status ? `AND status = '${query.status}' ` : ""}
+                            ${query.min_rate && query.max_rate ? `AND rate BETWEEN ${query.min_rate} AND ${query.max_rate}` : ""}
+                            ${query.min_rate && !query.max_rate ? `AND rate >= ${query.min_rate}` : ""}
+                            ${!query.min_rate && query.max_rate ? `AND rate <= ${query.max_rate}` : ""}
+                            ${query.min_weight && query.max_weight ? `AND weight BETWEEN ${query.min_weight} AND ${query.max_weight}` : ""}
+                            ${query.min_weight && !query.max_weight ? `AND weight >= ${query.min_weight}` : ""}
+                            ${!query.min_weight && query.max_weight ? `AND weight <= ${query.max_weight}` : ""}
+                            ${query.min_depth_value && query.max_depth_value ? `AND depth_value BETWEEN ${query.min_depth_value} AND ${query.max_depth_value}` : ""}
+                            ${query.min_depth_value && !query.max_depth_value ? `AND depth_value >= ${query.min_depth_value}` : ""}
+                            ${!query.min_depth_value && query.max_depth_value ? `AND depth_value <= ${query.max_depth_value}` : ""}
+                            ${query.min_table_value && query.max_table_value ? `AND table_value BETWEEN ${query.min_table_value} AND ${query.max_table_value}` : ""}
+                            ${query.min_table_value && !query.max_table_value ? `AND table_value >= ${query.min_table_value}` : ""}
+                            ${!query.min_table_value && query.max_table_value ? `AND table_value <= ${query.max_table_value}` : ""}
+                            ${query.min_measurement_height && query.max_measurement_height ? `AND measurement_height BETWEEN ${query.min_measurement_height} AND ${query.max_measurement_height}` : ""}
+                            ${query.min_measurement_height && !query.max_measurement_height ? `AND measurement_height >= ${query.min_measurement_height}` : ""}
+                            ${!query.min_measurement_height && query.max_measurement_height ? `AND measurement_height <= ${query.max_measurement_height}` : ""}
+                            ${query.min_measurement_width && query.max_measurement_width ? `AND measurement_width BETWEEN ${query.min_measurement_width} AND ${query.max_measurement_width}` : ""}
+                            ${query.min_measurement_width && !query.max_measurement_width ? `AND measurement_width >= ${query.min_measurement_width}` : ""}
+                            ${!query.min_measurement_width && query.max_measurement_width ? `AND measurement_width <= ${query.max_measurement_width}` : ""}
+                            ${query.min_measurement_depth && query.max_measurement_depth ? `AND measurement_depth BETWEEN ${query.min_measurement_depth} AND ${query.max_measurement_depth}` : ""}
+                            ${query.min_measurement_depth && !query.max_measurement_depth ? `AND measurement_depth >= ${query.min_measurement_depth}` : ""}
+                            ${!query.min_measurement_depth && query.max_measurement_depth ? `AND measurement_depth <= ${query.max_measurement_depth}` : ""}
+                            ${query.start_date && query.end_date
+                ? `AND created_at BETWEEN '${new Date(new Date(query.start_date as string).setUTCHours(0, 0, 0, 0)).toISOString()}' AND '${new Date(new Date(query.end_date as string).setUTCHours(23, 59, 59, 999)).toISOString()}'`
+                : ""}
+                              ${query.start_date && !query.end_date
+                ? `AND created_at >= '${new Date(new Date(query.start_date as string).setUTCHours(0, 0, 0)).toISOString()}'`
+                : ""}
+                              ${!query.start_date && query.end_date
+                ? `AND created_at <= '${new Date(new Date(query.end_date as string).setUTCHours(23, 59, 59, 999)).toISOString()}'`
+                : ""}
                     ORDER BY ${pagination.sort_by} ${pagination.order_by}
                     OFFSET
                       ${(pagination.current_page - 1) * pagination.per_page_rows} ROWS
