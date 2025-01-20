d9d13323d5cc7c64cffa45a1cb77bc8e8f74decc
Vihaa-Shreyas <136700117+Vihaa-Shreyas@users.noreply.github.com>
2025-01-20 16:52:25 +0530

    Update compare-diamonds.service.ts

--- src/services/user/compare-diamonds.service.ts
+++ src/services/user/compare-diamonds.service.ts
@@ -1,10 +1,17 @@
 import { Request } from "express";
-import { resSuccess } from "../../utils/shared-functions";
+import { resBadRequest, resSuccess } from "../../utils/shared-functions";
 import dbContext from "../../config/dbContext";
 import { QueryTypes } from "sequelize";
 
 export const compareDiamonds = async (req: Request) => {
     const { product_id } = req.body;
+
+    if (product_id.length === 0) {
+        return resBadRequest({
+            message: "Product ID is required."
+        })
+    }
+
     const compareDiamonds = await dbContext.query(
         `
             SELECT * FROM diamond_list where id in (${product_id.map((id: number) => `${id}`).join(",")}) 
