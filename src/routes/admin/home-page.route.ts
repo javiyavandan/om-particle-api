c4bca70b0dc7d264a6972a39977aa8d1fb524a60
Vihaa-Shreyas <136700117+Vihaa-Shreyas@users.noreply.github.com>
2025-02-13 19:21:32 +0530

    Update home-page.route.ts

--- src/routes/admin/home-page.route.ts
+++ src/routes/admin/home-page.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { adminAuthorization } from "../../middlewares/authenticate";
-import { addSectionFn, deleteSectionFn, getHomePageDataByIdFn, getSectionListFn, updateSectionFn, updateSectionStatusFn } from "../../controllers/admin/home-page.controller";
+import { addSectionFn, deleteSectionFn, getSectionFn, getSectionListFn, updateSectionFn, updateSectionStatusFn } from "../../controllers/admin/home-page.controller";
 import { reqMultiImageParser } from "../../middlewares/multipart-file-parser";
 
 export default (app: Router) => {
     app.post('/home-page-section', [reqMultiImageParser(["image", "hover_image"]), adminAuthorization], addSectionFn)
     app.put('/home-page-section/:section_id', [reqMultiImageParser(["image", "hover_image"]), adminAuthorization], updateSectionFn)
     app.get('/home-page-section/:section_type', [adminAuthorization], getSectionListFn)
-    app.get('/home-page-section/:section_type/:section_id', [adminAuthorization], getHomePageDataByIdFn)
+    app.get('/home-page-section/:section_type/:section_id', [adminAuthorization], getSectionFn)
     app.delete('/home-page-section/:section_id', [adminAuthorization], deleteSectionFn)
     app.patch('/home-page-section/:section_id', [adminAuthorization], updateSectionStatusFn)
 }
